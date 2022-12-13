a7efce38c21bbc1c51ac307ee76f435d5ad0654f
Stelios Petrakis <t8200139@aueb.gr>
2022-12-13 15:29:05 +0200

    /

--- vrp2/VRP_Model.py
+++ vrp2/VRP_Model.py
@@ -1,47 +1,49 @@
 import random
 import math
-import csv
 
 
 class Model:
 
+# instance variables
     def __init__(self):
         self.allNodes = []
         self.customers = []
         self.matrix = []
         self.capacity = -1
 
-
     def BuildModel(self):
-      
-        f = open("data2.txt", "r")
-        reader=csv.reader(f)
-        for row in reader:
-           cust = (Node(int(row[0]),int(row[1]),int(row[2]),int(row[3]),int(row[4])))
-           self.allNodes.append(cust)
-           self.customers.append(cust)
+        random.seed(1)
+        depot = Node(0, 50, 50, 0)
+        self.allNodes.append(depot)
+
+        self.capacity = 50
+        totalCustomers = 120
+
+        for i in range (0, totalCustomers):
+            x = random.randint(0, 100)
+            y = random.randint(0, 100)
+            dem = random.randint(1, 4)
+            cust = Node(i + 1, x, y, dem)
+            self.allNodes.append(cust)
+            self.customers.append(cust)
 
         rows = len(self.allNodes)
         self.matrix = [[0.0 for x in range(rows)] for y in range(rows)]
 
         for i in range(0, len(self.allNodes)):
-            
             for j in range(0, len(self.allNodes)):
                 a = self.allNodes[i]
                 b = self.allNodes[j]
                 dist = math.sqrt(math.pow(a.x - b.x, 2) + math.pow(a.y - b.y, 2))
                 self.matrix[i][j] = dist
 
-#ID,XCOORD,YCOORD,DEMAND,UNLOADING_TIME
 class Node:
-    def __init__(self, idd, xx, yy, dem,unload_time):
+    def __init__(self, idd, xx, yy, dem):
         self.x = xx
         self.y = yy
         self.ID = idd
         self.demand = dem
-        self.unload_time = unload_time
         self.isRouted = False
-   
 
 class Route:
     def __init__(self, dp, cap):
@@ -50,7 +52,4 @@ class Route:
         self.sequenceOfNodes.append(dp)
         self.cost = 0
         self.capacity = cap
-        self.load = 0  
-        
-
-
+        self.load = 0
