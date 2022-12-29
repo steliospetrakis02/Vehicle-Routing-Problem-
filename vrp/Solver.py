638bb24be3350ec1e9d46f8daa919ffe8a7499e9
Stelios Petrakis <t8200139@aueb.gr>
2022-12-29 23:25:02 +0200

    /

--- vrp/Solver.py
+++ vrp/Solver.py
@@ -6,8 +6,8 @@ class Solution:
     def __init__(self):
         self.cost = 0.0
         self.routes = []
+        self.test_routes = []
      
-
 class Solver:
     def __init__(self, m):
         self.allNodes = m.allNodes
@@ -26,7 +26,7 @@ class Solver:
         self.ReportSolution(self.sol)
         return self.sol
    
-
+    
     def SetRoutedFlagToFalseForAllCustomers(self):
         for i in range(0, len(self.customers)):
             self.customers[i].isRouted = False
@@ -35,24 +35,39 @@ class Solver:
             c.isRouted = False
 
     def ReportSolution(self, sol):
-      
+        f = open("res.txt", "w")
+        f.write("Cost:")
+        f.write("\n")
+        f.write(str(self.sol.cost))
+        f.write("\n")
+        f.write("Routes:")
+        f.write("\n")
+        f.write(str(len(sol.routes)))
+        f.write("\n")
+
         for i in range(0, len(sol.routes)):
             rt = sol.routes[i]
-            print(rt.sequenceOfNodes[0].ID, end=',')
+           # print(rt.sequenceOfNodes[0].ID, end=',')
+            f.write(str(rt.sequenceOfNodes[0].ID))
+            f.write(",")
             for j in range (0, len(rt.sequenceOfNodes)):
-                
                 if(rt.sequenceOfNodes[j].ID==0):
                     continue
-                if(rt.sequenceOfNodes[j].ID==100):
-                    continue
-                print(rt.sequenceOfNodes[j].ID, end=',')   
-
+                if(j == len(rt.sequenceOfNodes) - 1):    
+                    print(rt.sequenceOfNodes[j].ID)
+                    f.write(str(rt.sequenceOfNodes[j].ID))  
+                    break
                 
-            print(" ")
+                print(rt.sequenceOfNodes[j].ID , end=",")
+                f.write(str(rt.sequenceOfNodes[j].ID))
+              
+                f.write(",")
+           
+            f.write(" \n")
             
         SolDrawer.draw('Greedy', self.sol, self.allNodes)
-      
-    
+       
+        f.close()
     
     def ApplyNearestNeighborMethod(self,sol,route_number):
         
@@ -60,6 +75,7 @@ class Solver:
         minimumInsertionCost = 1000000
         rt=sol.routes[route_number]
         lastNodeInTheCurrentSequence = rt.sequenceOfNodes[-1]
+       
         
         for j in range (0, len(self.customers)):
             candidate = self.customers[j]
@@ -75,58 +91,88 @@ class Solver:
                 minimumInsertionCost = trialCost
         
         insertedCustomer = self.customers[indexOfTheNextCustomer]
+      
         rt.sequenceOfNodes.append(insertedCustomer)
         
         insertedCustomer.isRouted = True
         return sol
+    
+    def find_best_route(self,sol,route_number):
+        
+        indexOfTheNextCustomer = -1
+        minimumInsertionCost = 1000000
+        rt=sol.test_routes[route_number]
+        lastNodeInTheCurrentSequence = rt.sequenceOfNodes[-1]
+       
+        
+        for j in range (0, len(self.customers)):
+            candidate = self.customers[j]
+            if candidate.isRouted == True:
+                continue
+            
+            trialCost = self.distanceMatrix[lastNodeInTheCurrentSequence.ID][candidate.ID]
+            if(trialCost==0):
+                continue
+            
+            if (trialCost < minimumInsertionCost):
+                indexOfTheNextCustomer = j
+                minimumInsertionCost = trialCost
+        
+        insertedCustomer = self.customers[indexOfTheNextCustomer]
+      
+        rt.sequenceOfNodes.append(insertedCustomer)
+        
+        #self.ReportSolution(sol)
+        possible_route=self.CalculateTotalCost(sol)
+        rt.sequenceOfNodes.pop()
+     
+        return possible_route
 
     def start_all_routes(self):
         s = Solution()
         for i in range(14):
             rt = Route(self.depot, self.capacity)
             s.routes.append(rt)
+            s.test_routes.append(rt)
 
         return s
     
-    def distance(from_node, to_node):
-        dx = from_node.x - to_node.x
-        dy = from_node.y - to_node.y
-        dist = math.sqrt(dx ** 2 + dy ** 2)
-        return dist
-  
     
-    def calc_objective(self,nodes_sequence):
-        
-      rt_cumulative_cost = 0
-      tot_time = 0
-      for i in range(len(nodes_sequence) - 1):
-          from_node = nodes_sequence[i]
-          to_node = nodes_sequence[i+1]
-          tot_time += self.distance(from_node, to_node)
-          rt_cumulative_cost += tot_time
-          tot_time += to_node.serv_tim
-          
-      return rt_cumulative_cost
+    def CalculateTotalCost(self, sol):
+        total_cost = 0
+        for i in range(len(sol.routes)):
+            rt = sol.routes[i]
+            tot_time = 0       
+            rt_cumulative_cost = 0
+            for j in range(0,len(rt.sequenceOfNodes) - 1):
+                         
+                from_node = rt.sequenceOfNodes[j]            
+                to_node = rt.sequenceOfNodes[j + 1]            
+                tot_time += self.distanceMatrix[from_node.ID][to_node.ID]            
+                rt_cumulative_cost += tot_time            
+                tot_time += to_node.unload_time
+                       
+            total_cost += rt_cumulative_cost    
+        return total_cost
+    
+   
     
     def Solve(self,sol):
-        max_obj=100000000
-        for i in range(len(self.customers)):
-            
-            for j in range(14):
-                #s=self.calc_objective()
-                self.ApplyNearestNeighborMethod(sol,j)
-
         
-    def CalculateTotalCost(self, sol):
-        c = 0
-        for i in range(0, len(sol.sol)):
-            rt = sol.routes[i]
-            for j in range(0, len(rt.sequenceOfNodes) - 1):
-                a = rt.sequenceOfNodes[j]
-                b = rt.sequenceOfNodes[j + 1]
-                c += self.distanceMatrix[a.ID][b.ID]
-        return c
-
+        for i in range(len(self.customers)):
+            max_obj=100000000
+            for j in range(14):
+               
+                s=self.find_best_route(sol,j)
+             
+                if(s<max_obj):
+                    max_obj=s
+                    index=j
+       
+            self.ApplyNearestNeighborMethod(sol,index)
+        
+        sol.cost = self.CalculateTotalCost(sol)
+  
 
 
 m = Model()
